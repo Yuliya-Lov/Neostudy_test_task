f3ebc1e083c6dba7555a3a2d38185b3fca338cb6
Yuliya-Lov <jul-yaris@yandex.ru>
2023-05-11 10:25:53 +0300

    feat: layout main&cart

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+const headphones = [
+    {
+        img: "./assets/BYZS852I.png",
+        title: "Apple BYZ S852I",
+        price: 2927,
+        oldPrice: 3527,
+        rate:  4.7
+    },
+    {
+        img: "./assets/earPods-1.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate:  4.5
+    },
+    {
+        img: "./assets/earPods-2.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate:  4.5
+    },
+    {
+        img: "./assets/BYZS852I.png",
+        title: "Apple BYZ S852I",
+        price: 2927,
+        rate:  4.7
+    },
+    {
+        img: "./assets/earPods-1.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate:  4.5
+    },
+    {
+        img: "./assets/earPods-2.png",
+        title: "Apple EarPods",
+        price: 2327,
+        rate:  4.5
+    }
+]
+
+const wirelessHeadphones =[
+    {
+        img: "./assets/earPods-3.png",
+        title: "Apple EarPods",
+        price: 9527,
+        rate:  4.7
+    },
+    {
+        img: "./assets/GH-04.png",
+        title: "GERLAX GH-04",
+        price: 6527,
+        rate:  4.7
+    },
+    {
+        img: "./assets/BO4.png",
+        title: "BOROFONE BO4",
+        price: 7527,
+        rate:  4.7
+    },
+]
